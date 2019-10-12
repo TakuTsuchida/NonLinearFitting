bf2bef54fb704aef721fbdf1cba20e55b36140cb
土田拓 <takutsuchida@tsuchitatakunoMacBook-Pro.local>
2019-10-13 00:20:29 +0900

    feat(fix generalized linear models):

---
+++ core/config.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+# typing
+dataframe = pd.core.frame.DataFrame
+array = np.ndarray
--- core/fitting.py
+++ core/fitting.py
@@ -1,29 +1,33 @@
-import pandas as pd
-import numpy as np
+from config import dataframe, array
 
 
-def exp_data(df, columns: list, period: tuple):
-    start, end = period
+def exp_data(df: dataframe, columns: list, period: tuple) -> array:
+    start, end = period if period else df.index[0], df.index[-1]
     return df.loc[start:end, columns].values
 
 
-def obj_data(df, col: str, period: tuple):
+def obj_data(df: dataframe, col: str, period: tuple) -> array:
     import numpy as np
-    start, end = period
+    start, end = period if period else df.index[0], df.index[-1]
     return np.array(df[col][start:end])
 
 
-def least_squares(xs, y):
+def least_squares(xs: array, y: array) -> tuple:
     import sklearn.linear_model as LinearModel
     reg = LinearModel.LinearRegression()
     reg.fit(xs, y)
     return reg.coef_, reg.intercept_
 
 
-if __name__ == "__main__":
-    data = np.random.randint(0, 100, (100, 5))
-    df = pd.DataFrame(data, columns=list("ABCDE"))
-    period = (df.index[0], df.index[-1])
-    coef, const = least_squares(xs=exp_data(df, ['A', 'B'], period),
-                                y=exp_data(df, 'D', period))
-    print(coef, const)
+def ridge_regression(xs: array, y: array, alpha: float) -> tuple:
+    import sklearn.linear_model as LinearModel
+    reg = LinearModel.Ridge(alpha=alpha)
+    reg.fit(xs, y)
+    return reg.coef_, reg.intercept_
+
+
+def lasso_regression(xs: array, y: array, alpha: float) -> tuple:
+    import sklearn.linear_model as LinearModel
+    reg = LinearModel.Lasso(alpha=alpha)
+    reg.fit(xs, y)
+    return reg.coef_, reg.intercept_
